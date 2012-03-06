a620b47d8f8c59350aaa9c05516bf59e3a5e51dc
Sharry Stowell <sharry.@empowering-communities.org>
2012-03-06 15:54:12 +0000

    Adding sprite hacks

--- ie6.js
+++ ie6.js
@@ -3,5 +3,6 @@ $(function() {
 		$('.row div[class^="span"]:last-child').addClass('last-child');
 		$('.row div[class^="controls"] input:first-child').css('margin-left':'-3px'});
         $(':button[class="btn"], :reset[class="btn"], :submit[class="btn"], input[type="button"]').css({'height':'auto', 'width':'auto', 'margin-bottom' : '0px'});
+        $('[class^="icon-"], [class*=" icon-"]').addClass('icon-sprite');
 	}
 });
