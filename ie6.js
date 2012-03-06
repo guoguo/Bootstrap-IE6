9e2e60241cea94fbddc71e44367492b38c99dd12
TideSDK <sharry.stowell@gmail.com>
2012-03-06 15:08:00 +0000

    Adding buttons hacks

--- ie6.js
+++ ie6.js
@@ -2,5 +2,6 @@ $(function() {
 	if ( $.browser.msie && parseInt($.browser.version, 10) === 6) {
 		$('.row div[class^="span"]:last-child').addClass('last-child');
 		$('.row div[class^="controls"] input:first-child').css('margin-left':'-3px'});
+        $(':button[class="btn"], :reset[class="btn"], :submit[class="btn"], input[type="button"]').css({'height':'auto', 'width':'auto', 'margin-bottom' : '0px'});
 	}
 });
