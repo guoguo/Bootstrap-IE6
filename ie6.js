047156c694364b478065260aac806b9de4990adf
Sharry Stowell <sharry.@empowering-communities.org>
2012-03-07 10:49:18 +0000

    Adding pagination hacks

--- ie6.js
+++ ie6.js
@@ -4,5 +4,6 @@ $(function() {
 		$('.row div[class^="controls"] input:first-child').css('margin-left':'-3px'});
         $(':button[class="btn"], :reset[class="btn"], :submit[class="btn"], input[type="button"]').css({'height':'auto', 'width':'auto', 'margin-bottom' : '0px'});
         $('[class^="icon-"], [class*=" icon-"]').addClass('icon-sprite');
+        $('.pagination li:first-child a').addClass('pagination-first-child');
 	}
 });
