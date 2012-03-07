cfe571613b257f5d685025a971e46236644b99e6
Sharry Stowell <sharry.@empowering-communities.org>
2012-03-07 14:04:37 +0000

    Adding images support/layout issues

--- ie6.js
+++ ie6.js
@@ -1,9 +1,8 @@
 $(function() {
 	if ( $.browser.msie && parseInt($.browser.version, 10) === 6) {
 		$('.row div[class^="span"]:last-child').addClass('last-child');
-		$('.row div[class^="controls"] input:first-child').css('margin-left':'-3px'});
+        $('[class*="span"]').addClass('margin-left-20');
         $(':button[class="btn"], :reset[class="btn"], :submit[class="btn"], input[type="button"]').css({'height':'auto', 'width':'auto', 'margin-bottom' : '0px'});
         $('[class^="icon-"], [class*=" icon-"]').addClass('icon-sprite');
-        $('.pagination li:first-child a').addClass('pagination-first-child');
-	}
+        $('.pagination li:first-child a').addClass('pagination-first-child');	}
 });
